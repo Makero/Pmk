c52458a1dd84e5a7fa751c59584d0c6f379e5f01
maker <lcwjyouyi@163.com>
2018-04-12 09:01:54 +0800

    修改app名称

--- mk/settings.py
+++ mk/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_users.apps.AppUsersConfig',
+    'wechat.apps.AppUsersConfig',
 ]
 
 MIDDLEWARE = [
